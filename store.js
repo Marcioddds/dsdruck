8c7daf00c964e90ca71eecc5741be60b7b92ff91
Marcio Santos <marddds@hotmail.com>
2020-03-03 16:39:32 +0000

    ter

--- store.js
+++ store.js
@@ -94,17 +94,5 @@ function updateCartTotal() {
 }
 
 
-paypal.Buttons({
-  createOrder: function(data, actions) {
-    // This function sets up the details of the transaction, including the amount and line item details.
-    return actions.order.create({
-      purchase_units: [{
-        amount: {
-          value: 'cart-total-price'
-        }
-      }]
-    });
-  }
-}).render('#paypal-button-container');
 
 
